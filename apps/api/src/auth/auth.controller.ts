1376e2e44f1af9bd07461ef02fceae61a43e13a9
Tahsin Hassan <tahsin.hassan.700@gmail.com>
2026-09-16 12:07:31 +0600

    feat(auth): add CreateAuthDto with validation and remove unused create method

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -7,10 +7,7 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
+
 
   @Get()
   findAll() {
--- apps/api/src/auth/dto/create-auth.dto.ts
+++ apps/api/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAuthDto {}
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
